cbbcefb05116946c07bce0e2339f286ccaf8ded0
niphitphon8bit <niphitphon_nt@hotmail.com>
2023-11-18 12:26:00 +0700

    finish Simple Query's Chapter

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ export const server = new ApolloServer({
 const port = 3000;
 // 2
 server.listen({port}).then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀  Server ready at ${url} for sure!!`);
 });
--- src/schema.ts
+++ src/schema.ts
@@ -1,10 +1,11 @@
-import { makeSchema } from 'nexus'
-import { join } from 'path'
+import { makeSchema } from "nexus";
+import { join } from "path";
+import * as types from "./graphql";   // 1
 
 export const schema = makeSchema({
-    types: [], // 1
+    types,   // 2
     outputs: {
-        schema: join(process.cwd(), "schema.graphql"), // 2
-        typegen: join(process.cwd(), "nexus-typegen.ts"), // 3
+        typegen: join(process.cwd(), "nexus-typegen.ts"),
+        schema: join(process.cwd(), "schema.graphql"),
     },
-})
+});
